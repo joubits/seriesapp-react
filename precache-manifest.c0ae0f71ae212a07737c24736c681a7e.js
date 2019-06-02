89f130459fe1e2019f6dcc384c9cba0deef76188
joubits <joubits.dw@gmail.com>
2019-06-02 15:53:42 -0500

    Updates

---
+++ precache-manifest.c0ae0f71ae212a07737c24736c681a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4171cb8e79715f1e80a51c4c226278f",
+    "url": "/seriesapp-react/index.html"
+  },
+  {
+    "revision": "1fed90c02c60dbea9d3d",
+    "url": "/seriesapp-react/static/css/main.f42733ad.chunk.css"
+  },
+  {
+    "revision": "e6c1f5ed117acbd14ccf",
+    "url": "/seriesapp-react/static/js/2.f7d0a267.chunk.js"
+  },
+  {
+    "revision": "1fed90c02c60dbea9d3d",
+    "url": "/seriesapp-react/static/js/main.f3e3b397.chunk.js"
+  },
+  {
+    "revision": "7a27ac33b59d1b8a31a4",
+    "url": "/seriesapp-react/static/js/runtime~main.b447e633.js"
+  },
+  {
+    "revision": "07f010bd636fea8af07a61df9c7990ea",
+    "url": "/seriesapp-react/static/media/loader.07f010bd.gif"
+  }
+]);
